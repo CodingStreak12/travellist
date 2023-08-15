d015e45010ec2c1f783498c897cec9beb5078911
Nitin <nitinww53@gmail.com>
2023-08-15 18:35:28 -0400

    Start working on the main body of the travellist

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
+import { useState } from "react";
 const initialItems = [
   { id: 1, description: "Passports", quantity: 2, packed: false },
   { id: 2, description: "Socks", quantity: 12, packed: false },
+  { id: 3, description: "charger", quantity: 2, packed: true },
 ];
-
+let id = 4;
 export default function App() {
   return (
     <div className="app">
@@ -23,9 +25,38 @@ function Logo() {
 }
 
 function Form() {
+  const [desc, setDesc] = useState("");
+  const [quantity, setQuantity] = useState(1);
+  function handleSubmit(e) {
+    e.preventDefault();
+    if (!desc) {
+      return;
+    }
+
+    const newItem = { desc, quantity, id: id++, packed: false };
+    console.log(newItem);
+    setDesc("");
+    setQuantity(1);
+  }
   return (
-    <section className="add-form">
+    <section className="add-form-section">
       <h3>What do you need for your 😍 trip?</h3>
+      <form className="add-form" onSubmit={handleSubmit}>
+        <select value={quantity} onChange={(e) => setQuantity(+e.target.value)}>
+          {Array.from(Array(20), (_, index) => index + 1).map((i) => (
+            <option value={i} key={i}>
+              {i}
+            </option>
+          ))}
+        </select>
+        <input
+          type="text"
+          placeholder="Item..."
+          value={desc}
+          onChange={(e) => setDesc(e.target.value)}
+        ></input>
+        <button>Add</button>
+      </form>
     </section>
   );
 }
@@ -35,7 +66,7 @@ function PackingList() {
     <div className="list">
       <ul>
         {initialItems.map((item) => {
-          return <Item item={item} />;
+          return <Item item={item} key={item.id} />;
         })}
       </ul>
     </div>
@@ -43,7 +74,14 @@ function PackingList() {
 }
 
 function Item({ item }) {
-  return <li>{item.description}</li>;
+  return (
+    <li>
+      <span style={item.packed ? { textDecoration: "line-through" } : {}}>
+        {item.quantity} {item.description}
+      </span>
+      <button>❌</button>
+    </li>
+  );
 }
 
 function Stats() {
